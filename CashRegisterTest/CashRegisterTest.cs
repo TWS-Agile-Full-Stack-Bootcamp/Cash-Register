ba94979f23557fd0b6255b13b3a5e88c0d9b3553
Zhen She <neal.seah@gmail.com>
2020-11-05 01:47:44 +0800

    test(Question 2): use SpyPrinter to spy print of a real Printer

--- CashRegisterTest/CashRegisterTest.cs
+++ CashRegisterTest/CashRegisterTest.cs
@@ -9,13 +9,23 @@ namespace CashRegisterTest
 		public void Should_process_execute_printing()
 		{
 			//given
-			var printer = new Printer();
+			var printer = new SpyPrinter();
 			var cashRegister = new CashRegister(printer);
 			var purchase = new Purchase();
 			//when
 			cashRegister.Process(purchase);
 			//then
 			Assert.True(printer.HasPrinted);
+		}
+	}
+
+	internal class SpyPrinter : Printer
+	{
+		public bool HasPrinted { get; private set; }
+		public override void Print(string content)
+		{
+			base.Print(content);
+			HasPrinted = true;
 		}
 	}
 }
