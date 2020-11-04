691c32ffb7d0f3dc920e4f9d4ab03c33fb39df60
Zhen She <neal.seah@gmail.com>
2020-11-05 01:42:06 +0800

    test(Question 1): should print when processing

--- CashRegister/Printer.cs
+++ CashRegister/Printer.cs
@@ -2,9 +2,11 @@ namespace CashRegister
 {
 	public class Printer
 	{
+		public bool HasPrinted { get; private set; }
 		public void Print(string content)
 		{
 			// send message to a real printer
+			HasPrinted = true;
 		}
 	}
 }
