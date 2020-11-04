b8c026e1aca1600f1c4bb740e23514b672cb84db
Wu Xueheng <xhwu@thoughtworks.com>
2020-11-04 23:14:42 +0800

    test(question1): add test for CashRegister.Process

--- CashRegisterTest/CashRegisterTest.cs
+++ CashRegisterTest/CashRegisterTest.cs
@@ -1,6 +1,7 @@
 namespace CashRegisterTest
 {
 	using CashRegister;
+	using Moq;
 	using Xunit;
 
 	public class CashRegisterTest
@@ -8,13 +9,18 @@ namespace CashRegisterTest
 		[Fact]
 		public void Should_Process_Execute_Printing()
 		{
-			//given
-			var cashRegister = new CashRegister();
-			var purchase = new Purchase();
-			//when
-			cashRegister.Process(purchase);
-			//then
-			//verify that cashRegister.process will trigger print
+            //given
+            var mock = new Mock<Printer>();
+            mock.Setup(printer => printer.Print(It.IsAny<string>()));
+
+            var cashRegister = new CashRegister(mock.Object);
+            Purchase purchase = new Purchase();
+            //when
+            cashRegister.Process(purchase);
+
+            //then
+            //verify that cashRegister.process will trigger print
+            mock.Verify(mock => mock.Print(It.IsAny<string>()), Times.Once());
 		}
 	}
 }
