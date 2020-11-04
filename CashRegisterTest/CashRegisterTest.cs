16b89ff4063178e71b2451cacd2c5f4fde777331
Zhen She <neal.seah@gmail.com>
2020-11-05 02:43:57 +0800

    test(Question 4): process() should throw exception when printer is out of paper

--- CashRegisterTest/CashRegisterTest.cs
+++ CashRegisterTest/CashRegisterTest.cs
@@ -20,6 +20,29 @@ namespace CashRegisterTest
 			Assert.True(printer.HasPrinted);
 			Assert.Equal(purchase.AsString(), printer.ContentPrinted);
 		}
+
+		[Fact]
+		public void Should_throw_exception_when_printer_is_out_of_paper()
+		{
+			//given
+			var printer = new StubPrinter();
+			var cashRegister = new CashRegister(printer);
+			var purchase = new Purchase();
+			//when
+			//then
+			Assert.Throws<HardwareException>(() =>
+			{
+				cashRegister.Process(purchase);
+			});
+		}
+	}
+
+	internal class StubPrinter : Printer
+	{
+		public override void Print(string content)
+		{
+			throw new PrinterOutOfPaperException();
+		}
 	}
 
 	internal class StubPurchase : Purchase
